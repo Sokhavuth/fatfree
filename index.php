d7b63f8514ee992e4ce89e491aa2d61a001099f6
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-06-11 08:58:38 +0700

    Make it better

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require 'vendor/autoload.php';
 $f3 = \Base::instance();
-$f3->route('GET /',
-    function() {
-        echo 'Hello, world!';
+
+$f3->route('GET /', function() {
+    $view=new View;
+    echo $view->render('routes/index.php');
     }
 );
+
 $f3->run();
