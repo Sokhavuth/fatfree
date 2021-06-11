1af877f23e42f643b78b6b72360e4e5b68e427dc
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-06-11 11:19:02 +0700

    Make it better

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ require 'vendor/autoload.php';
 $f3 = \Base::instance();
 
 require_once('routes/index.php');
+require_once('routes/admin.php');
 
 $f3->run();
---
+++ routes/admin.php
@@ -0,0 +1,6 @@
+<?php
+//routes/admin.php
+
+$f3->route('GET /admin', function(){
+    echo 'Welcome to Backend!';
+});
